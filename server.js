4339209dc413d32668be937f29b7cc526cbe0dce
raghul292001 <133097101+raghul292001@users.noreply.github.com>
2023-09-16 20:24:29 +0530

    final

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Product = require('./models/productModel')
+const Product = require('./models/productModel');
 const app = express();
 
 
@@ -12,6 +12,17 @@ app.get('/',(req,res)=>{
 app.get('/blog',(req,res)=>{
  res.send('Hello Blog My Name is Raghu')
 })
+// Create a data in db
+app.post('/products',async(req,res)=>{
+  try{
+     const product= await Product.create(req.body)
+     res.status(200).json(product)
+ 
+  } catch(error){
+     console.log(error.message);
+     res.status(500).json({message:error.message})
+  }
+ })
 //Get all the data in db
 app.get('/products',async(req,res)=>{
     try{
@@ -34,18 +45,37 @@ app.get('/products/:id',async(req,res)=>{
     res.status(500).json({message:error.message});
   }
 })
-// Create a data in db
-app.post('/products',async(req,res)=>{
- try{
-    const product= await Product.create(req.body)
-    res.status(200).json(product)
-
- } catch(error){
-    console.log(error.message);
+//Update a data in db by id 
+app.put('/products/:id', async(req,res)=>{
+  try{
+    const {id}=req.params
+    const product=await Product.findByIdAndUpdate(id,req.body);
+    // We Cannot find any product in database 
+    if(!product){
+      return res.status(404).json({message:`cannot find any product with ID${id}`})
+    }
+    const updateProduct = await Product.findById(id)
+    res.status(200).json(updateProduct)
+  }catch(error){
     res.status(500).json({message:error.message})
- }
+  }
+
 })
-mongoose.set('strictQuery',false)
+//Delete the data from db
+app.delete('/products/:id', async(req,res)=>{
+  try{
+    const {id}=req.params
+      const product=await Product.findByIdAndDelete(id,req.body)
+      if(!product){
+      return res.status(404).json({message:`Cannot find any product with ID ${id}`})
+      }
+      res.status(200).json(product)
+  }catch(error){
+    res.status(500).json({message:error.message})
+  }
+
+})
+mongoose.set('strictQuery',false) // Use to avoid the unnessary error
 mongoose.connect('mongodb+srv://admin:admin@nodeapi.lfjkfvz.mongodb.net/Node-API')
 .then(()=>{
     app.listen(3000,()=>{
